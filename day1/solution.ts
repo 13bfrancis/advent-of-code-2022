0078bb290f3bf665c6f0fd476a2e9130dcd35baa
13bfrancis <40218571+13bfrancis@users.noreply.github.com>
2022-12-01 01:21:25 -0500

    complete day 1

---
+++ day1/solution.ts
@@ -0,0 +1,45 @@
+import { readFile } from "fs/promises";
+import path from "path";
+
+async function getFile() {
+  try {
+    const filePath = path.join(__dirname, "input.txt");
+    const input = await readFile(filePath);
+
+    return input.toString();
+  } catch (err) {
+    console.log(err);
+    return "";
+  }
+}
+
+function transformInput(input: string): number[][] {
+  return input
+    .split("\n\n")
+    .map((elf) => elf.split("\n").map((number) => parseInt(number)));
+}
+
+function getSums(elfs: number[][]) {
+  return elfs.map((elf) =>
+    elf.reduce((prev, curr) => {
+      return (prev += curr);
+    }, 0)
+  );
+}
+
+(async () => {
+  const elfs = transformInput(await getFile());
+  const sums = getSums(elfs);
+
+  // log answer for part 1
+  console.log("part 1", sums.sort((a, b) => a - b).slice(-1)[0]);
+
+  // log answer for part 2
+  console.log(
+    "part 2",
+    getSums(elfs)
+      .sort((a, b) => a - b)
+      .slice(-3)
+      .reduce((prev, curr) => (prev += curr), 0)
+  );
+})();
